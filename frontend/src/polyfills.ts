d17bf7c977c40f4b8767c6691e8e62c220e1222c
TUSHAR JAMBHULE <tusharjambhule1986@gmail.com>
2026-05-27 18:55:21 +0530

    Fix runtime process polyfill initialization order

--- frontend/src/polyfills.ts
+++ frontend/src/polyfills.ts
@@ -1,10 +1,10 @@
 (window as any).global = window;
 
-import { Buffer } from 'buffer';
-(window as any).Buffer = Buffer;
-
 import process from 'process';
 (window as any).process = process;
+
+import { Buffer } from 'buffer';
+(window as any).Buffer = Buffer;
 
 import * as assert from 'assert';
 (window as any).assert = assert;
